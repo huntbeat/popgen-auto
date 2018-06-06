654726d8a32af4be50cf859745a76878125a4755
Nhung Hoang <nhoang1@swarthmore.edu>
2018-06-06 11:46:16 -0400

    created dl directory

---
+++ dl/data_simulation/msprime_test.py
@@ -0,0 +1,17 @@
+"""
+Simulate data with msprime
+"""
+
+import msprime
+
+# simulate with n=25, N=10000, and L=3000
+tree_sequence = msprime.simulate(sample_size=25, Ne=10000, \
+    length=3000, mutation_rate = 1e-7, recombination_rate=1e-7)
+
+# see when the Tmrca changes
+for tree in tree_sequence.trees():
+    print(tree.interval, tree.tmrca(0,1)) # 0 and 1 here are the two sequences
+
+# see when mutations occur
+for variant in tree_sequence.variants():
+    print(variant.site.position, variant.genotypes, sep="\t")
