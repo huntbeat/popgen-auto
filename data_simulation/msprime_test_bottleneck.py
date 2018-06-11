edf23267aa626817ac9881746a38a42d68ed9900
Nhung Hoang <nhoang1@swarthmore.edu>
2018-06-11 15:37:48 -0400

    networks and data sim

--- data_simulation/msprime_test_bottleneck.py
+++ data_simulation/msprime_test_bottleneck.py
@@ -12,14 +12,17 @@ recovery = msprime.PopulationParametersChange(time=750,initial_size=10000)
 size_change_lst = [bottleneck, recovery]
 
 # simulate with n=2, N=10000, and L=10000
-tree_sequence = msprime.simulate(sample_size=2, Ne=10000, \
+tree_sequence = msprime.simulate(sample_size=10, Ne=10000, \
     length=10000, mutation_rate = 1e-7, recombination_rate=1e-7, \
     demographic_events=size_change_lst)
 
 # see when the Tmrca changes
-for tree in tree_sequence.trees():
-    print(tree.interval, tree.tmrca(0,1)) # 0 and 1 here are the two sequences
+#for tree in tree_sequence.trees():
+    #print(tree.interval, tree.tmrca(0,1)) # 0 and 1 here are the two sequences
 
+S = 0
 # see when mutations occur
 for variant in tree_sequence.variants():
-    print(variant.site.position, variant.genotypes, sep="\t")
+    #print(variant.site.position, variant.genotypes, sep="\t")
+    S += 1
+print(S)
--- dl/simple_network.py
+++ dl/simple_network.py
@@ -2,10 +2,12 @@
 
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
 import keras
 from keras.models import Sequential 
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Dense, Conv1D, Conv2D, Flatten, MaxPooling1D
 from keras import optimizers
 
 # fetch data 
@@ -19,18 +21,89 @@ with h5py.File(path,'r') as f:
   output = np.array(output_h5)
 data = np.concatenate((constant,bottleneck))
 
+# shuffle data before training
+data_shape_before = data.shape
+output_shape_before = output.shape
+s = np.arange(output.shape[0])
+data = data[s]
+output = output[s]
+assert(data_shape_before == data.shape)
+assert(output_shape_before == output.shape)
+
+# split into train/validation and test
+si = int(output.shape[0]*0.8)
+trainX = data[:si]
+trainY = output[:si]
+testX = data[si:]
+testY = data[si:]
+'''
+trainX = data[:100]
+trainY = output[:100]
+testX = data[100:200]
+testY = output[100:200]
+'''
+
 # neural network 
-n, L = data.shape[1:]
-data = np.reshape(data,(-1,n,L,1))
+n, L = trainX.shape[1:]
+trainX = np.reshape(trainX,(-1,n,L,1))
 model = Sequential()
-model.add(Conv2D(3, (5, 5), strides=(1,1), activation='relu', input_shape=(n, L, 1)))
-model.add(Conv2D(3, (4, 4), strides=(1,1), activation='relu'))
-model.add(Conv2D(3, (3, 3), strides=(1,1), activation='relu'))
+model.add(Conv2D(128, (5,5), activation='relu', input_shape=(n,L,1)))
+#model.add(Conv2D(128, (4,4), activation='relu'))
+model.add(Conv2D(128, (3,3), activation='relu'))
+#model.add(Conv1D(128, 5, activation='relu', input_shape=(n, L)))
+#model.add(MaxPooling1D())
+#model.add(Conv1D(128, 4, activation='relu'))
+#model.add(MaxPooling1D())
+#model.add(Conv1D(128, 5, activation='relu'))
+#model.add(MaxPooling1D())
+#model.add(Conv1D(128, 5, activation='relu'))
+#model.add(MaxPooling1D())
 model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+#model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='relu'))
+model.summary()
 
 SGD = optimizers.SGD()
 model.compile(optimizer=SGD, loss="binary_crossentropy", metrics=['accuracy'])
-history = model.fit(data, output, verbose=1, validation_split=0.2, epochs=10)
-plot(history)
+history = model.fit(trainX, trainY, verbose=1, validation_split=0.2, epochs=10)
 
+# confusion matrix on the test set
+num_samples, n2, L2 = testX.shape
+testX = np.reshape(testX,(-1,n2,L2,1))
+predictions = model.predict_classes(testX, verbose=1)
+predictions = np.reshape(predictions,(num_samples))
+true_labels = np.reshape(testY,(num_samples)) 
+orig_labels = [0,1] 
+
+print("\n\n**********************************")
+confuse = defaultdict(lambda: defaultdict(int))
+for lab1 in orig_labels:
+  for lab2 in orig_labels:
+    confuse[lab1][lab2] = 0
+for i in range(len(predictions)):
+  confuse[true_labels[i]][predictions[i]] += 1
+mat_str = "\n\t Prediction \n%11s" % sorted(confuse.keys())[0]
+for lab in sorted(confuse.keys())[1:]:
+  mat_str += "%6s" % lab
+mat_str += "\n"
+for row in sorted(confuse.keys()):
+  mat_str += "%5s " % row
+  for col in sorted(confuse[row].keys()):
+    mat_str += "%5d " % confuse[row][col]
+  mat_str += "\n"
+print(mat_str)
+print("**********************************")
+
+# save pred vs true to file
+
+
+# accuracy plot
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
