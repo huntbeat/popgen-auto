a1bfc0bd3b18e9ec1e74f0a89ec0034acfab82b1
Hyong Hark Lee <hlee6@swarthmore.edu>
2018-06-25 13:51:08 -0400

    fixed

--- dl/weight_analysis.py
+++ dl/weight_analysis.py
@@ -2,6 +2,8 @@ from analyze_network import get_weights
 import numpy as np
 import matplotlib.pyplot as plt
 
+wm_abs_dif_list = []
+
 for arguments in [('/home/nhoang1/saralab/popgen-hmm-dl/dl/TD_pop/TD_pop_model.hdf5', 'pop', 'TD', 'TD'), 
                   ('/home/nhoang1/saralab/popgen-hmm-dl/dl/dupl_pop/dupl_pop_model.hdf5', 'pop_1', 'TD_1', 'duplicate'),
                   ('/home/nhoang1/saralab/popgen-hmm-dl/dl/rand_pop/rand_pop_model.hdf5', 'pop_2', 'TD_2', 'random')]:
@@ -22,6 +24,8 @@ for arguments in [('/home/nhoang1/saralab/popgen-hmm-dl/dl/TD_pop/TD_pop_model.h
     wm_dif = wm - wm_pop_orig
     wm_abs_dif = np.absolute(wm_dif)
 
+    wm_abs_dif_list.append(np.sort(np.sum(wm_abs_dif, axis=1)/3.0))
+
     plt.figure(1)
     plt.plot(np.arange(1,257),np.sort(wm_abs_dif, axis=0))
     plt.legend(['< 0, nat_sel','= 0, constant','> 0, bottleneck'])
@@ -34,3 +38,8 @@ for arguments in [('/home/nhoang1/saralab/popgen-hmm-dl/dl/TD_pop/TD_pop_model.h
     plt.show()
     
     plt.close()
+
+plt.plot(np.arange(1,257),wm_abs_dif_list)
+plt.title('Summed weight abs dif for three outputs')
+plt.legend(['TD', 'DP', 'RN'])
+plt.show()
